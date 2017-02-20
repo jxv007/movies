1bf666c0f3e39ebe1ba4698681f183f63c2b922e
jxv007 <jxv007@163.com>
2017-02-21 06:47:00 +0800

    完善解析影片详情-未完成

--- app/controllers/Spider.js
+++ app/controllers/Spider.js
@@ -5,6 +5,7 @@ var request = require('request');
 var iconv = require('iconv-lite');
 var BufferHelper = require('bufferhelper');
 var Promise = require('bluebird');
+var Movie = require('Movie');
 
 var i = 0;
 var baseUrl = 'http://www.dy2018.com';
@@ -45,6 +46,48 @@ function parseList ( html ) {
     });
 };
 
+var MovieSchema = {
+    title: String          // 标　　题
+  , name: String           // 译　　名　奇异博士/斯特兰奇博士/史特兰奇博士/奇怪博士/怪奇医生/奇异医生/史奇医生
+  , oldName: String        // 片　　名　Doctor Strange
+  , year: Number           // 年　　代　2016
+  , country: String        // 国　　家　美国
+  , category:{             // 类　　别　动作/科幻/奇幻/冒险
+        type:ObjectId
+      , ref:'Category'
+    }               
+  , language: String        // 语　　言　英语
+  , subtitle: String        // 字　　幕　中英双字幕
+  , imdb: String            // IMDb评分  7.8/10 from 199,761 users
+  , douban: String          // 豆瓣评分　7.8/10 from 184,372 users
+  , format: String          // 文件格式　x264 + aac
+  , videoSize: String       // 视频尺寸　1280 x 720
+  , fileSize: String        // 文件大小　1CD
+  , videoLength: String     // 片　　长　115分钟
+  , director: String        // 导　　演　斯科特·德瑞克森 Scott Derrickson
+  , starring: []            // 主　　演　本尼迪克特·康伯巴奇 Benedict Cumberbatch
+  , intro: String           // 简　　介
+  , award: String           // 获奖情况
+  , poster: String          // 海报图片地址
+  , imgs: String            // 影片截图
+  , trailer: String         // 预告片地址
+  , state: {                // 状态：0 - 草稿；1 - 待审核； 2 - 审核通过； 3 - 审核未通过
+      type: Number,
+      default: 0 
+    }         
+  , meta:{
+      createAt:{
+        type:Date,
+        default:Date.now()
+      }
+    , updateAt:{
+        type:Date,
+        default:Date.now()
+      }
+    }
+};
+
+
 // 解析电影详情网页内容
 // TODO: 解析影片数据，存储到数据库，并显示出来
 
@@ -66,13 +109,53 @@ function filterMovies ( html ) {
     // } 
 
     var movie = {
-        title: title,
-        introduce: introduce,
-        download: download
-    };
+          title: title 
+        , name: name 
+        , oldName: oldName 
+        , year: year
+        , country: country 
+        , category:{
+              type:ObjectId
+            , ref:'Category'
+            }               
+        , language: language 
+        , subtitle: subtitle  
+        , imdb: imdb  
+        , douban: douban   
+        , format: format     
+        , videoSize: videoSize 
+        , fileSize: fileSize   
+        , videoLength: videoLength 
+        , director: director 
+        , starring: []    
+        , intro: intro      
+        , award: award   
+        , poster: poster   
+        , imgs: imgs   
+        , trailer: trailer  
+        , state: {        
+            type: Number,
+            default: 0 
+        } 
+        , meta:{
+            createAt:{
+                type:Date,
+                default:Date.now()
+            }
+            , updateAt:{
+                type:Date,
+                default:Date.now()
+            }
+            }
+        };
 
     printData(movie);
 };
+
+function saveMovie (movie) {
+    // TODO:调用 Movie.save 保存影片到数据库中，保存图片到本地服务器
+
+}
 
 // 抓取指定 url 网页内容，并返回 Promise 实例
 function getPageAsync ( url ) {
